ea1ad20aaa5382c251879a32490afa8e92508e35
Ruslan Sadykov <S.Ru5C55an.N@gmail.com>
2020-05-29 03:17:24 +1000

    init

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,8 @@ import '../scss/style.scss';
 
 import 'bootstrap';
 
-require.context('../blocks/', true, /\.(png|gif|svg|jpg)$/);
-require.context('../img/', true, /\.(png|gif|svg|jpg)$/);
+require.context('../blocks/', true, /\.(png|gif|svg|jpg|jpeg)$/);
+require.context('../img/', true, /\.(png|gif|svg|jpg|jpeg)$/);
 
 $('#btn-tooltip').tooltip();
 
